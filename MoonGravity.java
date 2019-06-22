08a87dca76a6411998390aa729abde898a034310
JeongHee Lee <jeonghee.lee@live.co.kr>
2019-06-22 15:01:57 +0800

    IC-2019.1.3 <jeong@jeong-Predator-G3-571 Overwrite remote https://github.com/jeonghee-lee/java to local

--- MoonGravity.java
+++
@@ -1,8 +0,0 @@
-class MoonGravity {
-	public static void main ( String[] arg) {
-		double Earthweigt, MoonWeight;
-		Earthweigt = 64;
-		MoonWeight = Earthweigt * 0.17;
-		System.out.println("Earth weight " + Earthweigt + "kg is equivalent to " + MoonWeight +" moon-kg!");
-		}
-}
--- Sound.java
+++
@@ -1,9 +0,0 @@
-class Sound {
-	public static void main ( String[] args ) {
-	double distance;
-	for (int sec=1;sec <=10;sec++) {
-		distance = sec * 335.28;
-		System.out.println("The lighting after sound of " + sec + " seconds is approximately " + distance + " meters away from you" );
-	}
-}
-}
